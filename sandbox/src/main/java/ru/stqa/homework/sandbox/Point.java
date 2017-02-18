7aadd7ee4974f53ebbbaa981b05d5a45cd507ca0
Bughuntress <qazakhidat@gmail.com>
2017-02-18 10:23:02 +0300

    1л.2з: Создан класс Point

---
+++ sandbox/src/main/java/ru/stqa/homework/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.homework.sandbox;
+
+/**
+ * Created by Zakhidat on 15.02.2017.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+      }
+}
