6b390925cc2cf65fd8774692c56547a00cbcf4d2
Bughuntress <qazakhidat@gmail.com>
2017-02-19 00:32:58 +0300

    1л2з: Создание метода

--- sandbox/src/main/java/ru/stqa/homework/sandbox/Coordinate.java
+++ sandbox/src/main/java/ru/stqa/homework/sandbox/Coordinate.java
@@ -7,10 +7,8 @@ public class Coordinate {
     public static void main(String[] args){
       Point p1 = new Point(8,2);
       Point p2 = new Point(22,3);
-    System.out.println("Расстояние между координатами x, равные (" + p1.x +";"+p1.y+") и координатой у, равной ("+ p2.x+";"+p2.y+") равно "+distance(p1, p2));
+    System.out.println("Расстояние между координатами x, равные (" + p1.x +";"+p1.y+") и координатой у, равной ("+ p2.x+";"+p2.y+") равно "+ Point.distance(p1,p2));
 
   }
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.sqrt(p2.x-p1.x)+Math.sqrt(p2.y-p1.y));
-  }
+
 }
--- sandbox/src/main/java/ru/stqa/homework/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/homework/sandbox/Point.java
@@ -11,4 +11,8 @@ public class Point {
     this.x = x;
     this.y = y;
       }
+  public static double distance(Point p1, Point p2){
+
+    return Math.sqrt(Math.sqrt(p2.x-p1.x)+Math.sqrt(p2.y-p1.y));
+  }
 }
