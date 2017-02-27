2c63366834f7428c2b08277e6879fc89313ef86b
Bughuntress <qazakhidat@gmail.com>
2017-02-27 20:56:02 +0300

    2л5з: Сделан общий базовый класс для тестов TestBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class ContactDeletionTests extends TestBase {
+
+    
+
+    
+    @Test
+    public void testContactDeletion() {
+        gotoContactPage();
+        selectContact();
+        selectedDeleteContact();
+        closePopup();
+
+
+    }
+
+
+}
