950e1d15bdbcb99d0b66c124f95cf0c3b88b31b6
Bughuntress <qazakhidat@gmail.com>
2017-03-02 21:06:55 +0300

    3л7з: Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.tests.SessionHelper;
 
 import java.util.concurrent.TimeUnit;
@@ -9,14 +13,27 @@ import java.util.concurrent.TimeUnit;
  * Created by Zakhidat on 27.02.2017.
  */
 public class ApplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private ContactHelper contactHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
+    if (browser == BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    }
+    else if (browser == BrowserType.CHROME){
+      wd = new ChromeDriver();
+    }else if (browser == BrowserType.IE){
+      wd = new InternetExplorerDriver();
+    }
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
