44548abcda466eeb8f3e2980e61f90325aa1dc78
Bughuntress <qazakhidat@gmail.com>
2017-03-28 22:47:59 +0300

    4в9л: Лямда-выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -20,7 +20,7 @@ public class ContactModificationTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData("Белль", "Джейсон", "Красавица", "Красавица и Чудовище", "Диснейлэнд", "1, Заколдованный Замок, Волшебный Лес", "+22222222", "22, Дом Отца, Маленькая деревушка", null);
+    ContactData contact = new ContactData(before.get(before.size()-1).getId(), "Белль", "Джейсон", "Красавица", "Красавица и Чудовище", "Диснейлэнд", "1, Заколдованный Замок, Волшебный Лес", "+22222222", "22, Дом Отца, Маленькая деревушка", null);
     app.getContactHelper().fillContactCreation(contact,false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToContactPage();
