1d1875a69abadcee448f2eeb379ff19a201b3526
Bughuntress <qazakhidat@gmail.com>
2017-04-04 22:19:26 +0300

    5л10в: Режем строки+регулярные выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactPhoneTests.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by Zakhidat on 01.04.2017.
  */
@@ -13,6 +16,12 @@ public class ContactPhoneTests extends TestBase {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
-    
+    assertThat(contact.getHometel(), equalTo(cleaned(contactInfoFromEditForm.getHometel())));
+    assertThat(contact.getMobile(), equalTo(cleaned(contactInfoFromEditForm.getMobile())));
+    assertThat(contact.getWorktel(), equalTo(cleaned(contactInfoFromEditForm.getWorktel())));
+
+    }
+  public String cleaned(String phone) {
+    return phone.replaceAll("\\s","").replaceAll("[-()]","");
   }
 }
